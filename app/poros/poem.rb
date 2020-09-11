d189a0237168959f2d5bb1278d0da9989cf32c33
Gaby Mendez <gmendez90@gmail.com>
2020-09-11 12:36:07 -0400

    Refactor Poem poro

--- app/poros/poem.rb
+++ app/poros/poem.rb
@@ -1,16 +1,19 @@
 class Poem
-  attr_reader :title, :author, :full_text
+  SEARCH = SearchFacade.new
+  attr_reader :title, :author, :full_text, :tones
   def initialize(poem_params)
     @title = poem_params[:title]
     @author = poem_params[:author]
     @full_text = poem_params[:lines].join("\n")
-    @search = SearchFacade.new
+    @tones = SEARCH.document_tones(@full_text).map do |tone|
+      tone[:tone_name]
+    end
   end
 
-  def tones
-    tones = @search.document_tones(@full_text)
-    tones.map do |tone|
-      tone[:tone_name]
-    end 
-  end
+  # def tones
+  #   tones = @search.document_tones(@full_text)
+  #   tones.map do |tone|
+  #     tone[:tone_name]
+  #   end
+  # end
 end
