3d8dcc2c3b46b94cc1cc299952c98ed8d3cf9721
Gaby Mendez <gmendez90@gmail.com>
2020-09-11 12:02:55 -0400

    Update SearchController#index, add private method to create searchFacade connection

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,13 @@
 class SearchController < ApplicationController
+
   def index
-    
+    require "pry"; binding.pry
+    @poems = search.top_10_poems_for(params[:author])
+  end
+
+  private
+
+  def search
+    SearchFacade.new
   end
 end
